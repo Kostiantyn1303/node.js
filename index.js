a1005a7088a3f8477ffd1741a99abafcb1482594
Kostiantyn1303 <kosty13humera@gmail.com>
2023-07-08 23:13:41 +0300

    done

---
+++ index.js
@@ -0,0 +1,38 @@
+import contactsService from "./db/contacts.js";
+import yargs from "yargs";
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await contactsService.listContacts();
+      return console.log(allContacts);
+
+      break;
+
+    case "get":
+      const oneContact = await contactsService.getContactById(id);
+      return console.log(oneContact);
+      break;
+
+    case "add":
+      const newContact = await contactsService.addContact({
+        name,
+        email,
+        phone,
+      });
+      return console.log(newContact);
+      break;
+
+    case "remove":
+      const deleteContact = await contactsService.removeContact(id);
+      return console.log(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};
+
+const { argv } = yargs(process.argv.slice(2));
+
+invokeAction(argv);
