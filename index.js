c8f78b8d1a715be579be1efc5ca68d7359de6424
Kostiantyn1303 <kosty13humera@gmail.com>
2023-07-11 15:56:31 +0300

    change console.log on console.table

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const invokeAction = async ({ action, id, name, email, phone }) => {
   switch (action) {
     case "list":
       const allContacts = await contactsService.listContacts();
-      return console.log(allContacts);
+      return console.table(allContacts);
 
       break;
 
